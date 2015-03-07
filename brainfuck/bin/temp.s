8a5813a3db01c433195b901905f196da77f310a9
Quey-Liang Kao <s101062801@m101.nthu.edu.tw>
2015-03-07 23:24:32 +0800

    [ and ] remains
    the last mile for the first brainfuck compiler in brainfuck

---
+++ brainfuck/bin/temp.s
@@ -0,0 +1,648 @@
+.section    .text
+_bicib:
+mov %rsp, %rbx
+sub $0x24, %rbx
+mov $0x1, %rdx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+mov %rbx, %rsi
+mov $0x1, %rdi
+mov $0x1, %rax
+syscall
+dec %rbx
+retq
+.global        _bicib
+.end
